1e88a26ea335d11b10ea0525d6742d99b183dad8
rpycgo <skyd0608@gmail.com>
2024-11-15 17:02:10 +0900

    implements: Member

--- src/banking.py
+++ src/banking.py
@@ -4,3 +4,10 @@ class InterestStrategy(ABC):
     @abstractmethod
     def apply_interest(self, money):
         pass
+
+
+class Member(InterestStrategy):
+    def apply_interest(self, money):
+        print('No Discount')
+
+        return money
