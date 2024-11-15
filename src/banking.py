e606b75a06d768e4e2098e0a13eb1055a5639a92
rpycgo <skyd0608@gmail.com>
2024-11-15 17:08:59 +0900

    feat: Implements NormalMember
    ~~~~
    ~~~~
    
    ticket: resolve KAN-12

--- src/banking.py
+++ src/banking.py
@@ -11,3 +11,11 @@ class Member(InterestStrategy):
         print('No Discount')
 
         return money
+
+
+class NormalMember(InterestStrategy):
+    def apply_interest(self, money):
+        interest = money * 0.022
+        print(f'NormalMember interest: {interest}')
+
+        return money + interest
